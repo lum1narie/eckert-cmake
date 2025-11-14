73d7f3ad2614a9afafe1d879954f0be2f623af5a
lum1narie <sn.lum.fallenrose.aol@gmail.com>
2025-11-14 14:28:01 +0900

    feat: :sparkles: nix integration

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A flake for building eckert";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        eckertDrv = pkgs.stdenv.mkDerivation {
+          src = self;
+          name = "eckert";
+
+          nativeBuildInputs = with pkgs; [
+            cmake
+            ninja
+          ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp eckert $out/bin/
+          '';
+        };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        packages = {
+          default = eckertDrv;
+          eckert = eckertDrv;
+        };
+      });
+}
